0e8d9b1d874524d93951c50419a64e4ac91b2926
tarun <tarun@competentgroove.com>
2022-02-26 07:07:52 +0000

    my code 1

--- hello.java
+++ hello.java
@@ -1,9 +1,7 @@
-public class hello{
-
+public class Hello{
 	public static void main(String[] args){
-
 		for(int i=1;i<=10;i++){
-			System.out.println("hello world ... "+i);
+			System.out.println("Hello world ... "+i);
 		}
 	}
 }
